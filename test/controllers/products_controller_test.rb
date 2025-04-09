d25d4b4aba76f5d2710a5223f7120722a84b6ef8
Amanda Jones <privyet.amanda@gmail.com>
2025-04-09 17:14:57 -0500

    adjusting formatting of params in products controller test; revising/adding some comments for my own understanding

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -18,7 +18,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference("Product.count") do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
+      post products_url, params: { 
+        product: { 
+          description: @product.description, 
+          image_url: @product.image_url, 
+          price: @product.price, 
+          title: @title
+        }
+      }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -39,10 +46,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
-  test "can't delete product in cart"
-    assert_difference("Product.count", 0) do
+  test "can't delete product in cart" do
+    assert_difference("Product.count", 0) 
       delete product_url(products(:two))
-    end
     
       assert assert_redirected_to products_url
   end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,19 +1,18 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  fixtures :products # causes products.yml fixture file to be used for tests
+  # loads test data
+  fixtures :products 
 
-  # all fields populated
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
     assert product.errors[:title].any?
     assert product.errors[:description].any?
     assert product.errors[:price].any?
-    assert product.errors[:image_url].any?
   end
 
-  # price validation - this COULD be separated into 3 different test methods
+  # this COULD be separated into 3 different test methods
   test "product price must be positive" do
     product = Product.new(title:        "My Book Title",
                           description:  "yyy",
